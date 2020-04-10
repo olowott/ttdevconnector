886b88c931d79f2a95d11779e6351a915fcf48cd
Tope Olowookere <olowott@gmail.com>
2020-04-10 06:01:11 +0100

    Adding colors to log outputs in terminal

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
+const colors = require('colors'); // coloring the logs
 
 const connectDB = async () => {
   try {
@@ -9,7 +10,7 @@ const connectDB = async () => {
       useUnifiedTopology: true,
     });
 
-    console.error('MongoDB connected....');
+    console.error('MongoDB connected....'.yellow.underline);
   } catch (err) {
     console.log(err.message);
     // exit process with failure
--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express'); // init a server
 const connectDB = require('./config/db'); //connection to MongoDatabase
+const colors = require('colors'); // coloring the logs
 
 //get server to start
 const app = express();
 // port server will run on
 const PORT = process.env.PORT || 5000;
 // specifing port the server will be on or listen to
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`.cyan.bold));
 // single end point for the API
 app.get('/', (req, res) => res.send('API running'));
 //connect Databse
